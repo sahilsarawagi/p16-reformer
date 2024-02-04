deae106ffcaaa17f0a1f26bde58bd42ca04418f7
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-04 17:20:12 +0530

    Create user creation form and update UsersController#create action
    - Built form for new user creation with appropriate attributes and security measures
    - Modified UsersController#create to handle user creation securely
    - Implemented user_params method for secure parameter handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(allowed_post_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def allowed_post_params
+    params.require(:user).permit(:username,:email,:password)
   end
 end
