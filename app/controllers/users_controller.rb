e37401faf476c7d1e829009bb0c4f414d63d019e
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-05 00:39:26 +0530

    Implement user editing functionality
    - Updated routes added :edit and :update
    - Added edit and update action in user_controller.
    - Created the Edit view
    - Modified the form view to display error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-   @user = User.new(allowed_post_params)
+   @user = User.new(allowed_user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -14,8 +14,21 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   private
-  def allowed_post_params
+  def allowed_user_params
     params.require(:user).permit(:username,:email,:password)
   end
 end
